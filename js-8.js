c6e201137bf06d9cf72d42341278ae5f30450998
aviraj5121 <avirajnimja12@gmail.com>
2023-02-14 20:26:18 +0530

    TO-DO list

--- js-8.js
+++ js-8.js
@@ -3,7 +3,7 @@ let todoList = document.getElementById("todo-list");
     function add() {
         let list = document.querySelector("#item").value;
         var todoItem = document.createElement("li");
-        todoItem.innerHTML = list + '<button onclick="done(this)" id="tick">done</button>';
+        todoItem.innerHTML = list + '<button onclick="done(this)" id="tick">done</button> <button onclick = "remove()" id = "delete">Delete</button';
         todoList.appendChild(todoItem);
         document.querySelector("#item").value = "";
     }
@@ -16,4 +16,12 @@ let todoList = document.getElementById("todo-list");
 
     function done(element) {
         element.parentElement.classList.add("done");
+    }
+    function removeall()
+    {
+        var parent = document.getElementById("todo-list");
+        while(parent.firstChild)// to chek is all child are removed or not
+        {
+            parent.removeChild(parent.firstElementChild);
+        }
     }
